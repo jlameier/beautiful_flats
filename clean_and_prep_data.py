671749fb12a8cf464fbb2f0b95985e0475b87813
jla <jlameier@posteo.de>
2021-07-22 00:48:03 +0200

    having a numerical-vector-style pandas dataframe object

---
+++ clean_and_prep_data.py
@@ -0,0 +1,111 @@
+"""
+
+load df
+transform text and prices, to decimals
+clear empties
+make new df containing ID, price, space, rooms, level, number furnishing
+save df
+
+"""
+
+import pandas as pd
+import re
+import numpy as np
+
+df = pd.read_csv("all_pd_ads_20210721-202013.csv")
+
+# remove column views and kill lines with NaN
+df = df.drop(columns='num_views')
+df = df.dropna()
+
+# convert to numbers
+df['price'] = df['price'].astype(str)
+
+# convert prices to  int
+def get_price_int(df):
+    list_return = []
+    for index, row in df.iterrows():
+        obj_value = row['price']
+        string_value = re.findall(r'\d+', obj_value)
+        if string_value:
+            string_value = ''.join(string_value)
+        #    string_value = string_value[0]+string_value[1]
+            num_value = int(string_value)
+            if num_value > 20000000:
+                list_return.append(np.nan)
+            else:
+                list_return.append(num_value)
+            # list_return.append(num_value)
+        else:
+            list_return.append(np.nan)
+    return list_return
+
+
+def get_space_int(df):
+    list_return = []
+    for index, row in df.iterrows():
+        obj_value = row['space']
+        string_value = re.findall(r'\d+', obj_value)
+        if string_value:
+            #string_value = ''.join(string_value)
+            #    string_value = string_value[0]+string_value[1]
+            num_value = int(string_value[0])
+            list_return.append(num_value)
+        else:
+            list_return.append(np.nan)
+    return list_return
+
+
+def get_rooms_int(df):
+    list_return = []
+    for index, row in df.iterrows():
+        obj_value = row['num_rooms']
+        string_value = float(obj_value.replace(',', '.'))
+        list_return.append((string_value))
+    return list_return
+
+
+def get_level_int(df):
+    list_return = []
+    for index, row in df.iterrows():
+        obj_value = row['level']
+        string_value = re.findall(r'\d+', obj_value)
+        if string_value:
+            string_value = ''.join(string_value)
+            num_value = int(string_value)
+            if num_value > 100:
+                list_return.append(np.nan)
+            else:
+                list_return.append(num_value)
+        else:
+            list_return.append(np.nan)
+    return list_return
+
+
+def get_num_furnishing(df):
+    list_return = []
+    for index, row in df.iterrows():
+        obj_value = row['furnishing']
+        string_values = obj_value.split('\n')
+        if string_values:
+            test= len(list(filter(None, string_values)))
+            list_return.append(len(list(filter(None, string_values))))
+        else:
+            list_return.append(np.nan)
+    return list_return
+
+
+# construct df from transformed values
+d_num = {}
+d_num['furnishing'] = get_num_furnishing(df)
+d_num['level'] = get_level_int(df)
+d_num['price'] = get_price_int(df)
+d_num['id'] = df['id']
+d_num['rooms'] = get_rooms_int(df)
+d_num['space'] = get_space_int(df)
+
+
+df_num = pd.DataFrame(d_num)
+df_num = df_num.dropna()
+df.to_csv('num_items_clean.csv')
+print("done")
--- get_content.py
+++ get_content.py
@@ -5,6 +5,7 @@ get soap from site - done
 process soap -done
 add to df - done
 dump df into sqlite3
+todo filter for multiple instances of the same link
 
 """
 import csv
@@ -29,7 +30,7 @@ def get_links(file, baselink):
                 list_complete_links.append(baselink + link)
     # shuffle links for obfuscation of crawling
     shuffle(list_complete_links)
-    return list_complete_links
+    return list(set(list_complete_links))
 
 
 def get_soup(url):
@@ -50,22 +51,66 @@ def extract_info_d(soup, link):
     # Equipment, long_description
     d_data = {}
     d_data['link'] = link
-    d_data['headline'] = soup.find(itemprop="name").get_text()
-    d_data['price'] = soup.find(id="viewad-price").get_text()
-    d_data['address'] = soup.find(itemprop="locality").get_text()
-    d_data['published'] = soup.find("div", id="viewad-extra-info").get_text()[1:11]
-    d_data['num_views'] = soup.find(id="viewad-cntr-num").get_text() # TODO returns empty ??
-    d_data['id'] = soup.find("div", {"id": "viewad-extra-info"}).text[28::]
-    d_data['space'] = soup.find_all(class_="addetailslist--detail--value")[0].get_text()
-    d_data['num_rooms'] = soup.find_all(class_="addetailslist--detail--value")[1].get_text()
-    d_data['level'] = soup.find_all(class_="addetailslist--detail--value")[2].get_text()
-    d_data['type'] = soup.find_all(class_="addetailslist--detail--value")[3].get_text()
+    try:
+        d_data['headline'] = soup.find(itemprop="name").get_text()
+    except:
+        d_data['headline'] = 0
+        print(link)
+    try:
+        d_data['price'] = soup.find(id="viewad-price").get_text()
+    except:
+        d_data['price'] =''
+        print(link)
+    try:
+        d_data['address'] = soup.find(itemprop="locality").get_text()
+    except:
+        d_data['address'] =''
+        print(link)
+    try:
+        d_data['published'] = soup.find("div", id="viewad-extra-info").get_text()[1:11]
+    except:
+        d_data['published'] =''
+        print(link)
+    try:
+        d_data['num_views'] = soup.find(id="viewad-cntr-num").get_text() # TODO returns empty ??
+    except:
+        d_data['num_views'] =''
+        print(link)
+    try:
+        d_data['id'] = soup.find("div", {"id": "viewad-extra-info"}).text[28::]
+    except:
+        d_data['id'] = ''
+        print(link)
+    try:
+        d_data['space'] = soup.find_all(class_="addetailslist--detail--value")[0].get_text()
+    except:
+        d_data['space'] =''
+        print(link)
+    try:
+        d_data['num_rooms'] = soup.find_all(class_="addetailslist--detail--value")[1].get_text()
+    except:
+        d_data['num_rooms'] = ''
+        print(link)
+    try:
+        d_data['level'] = soup.find_all(class_="addetailslist--detail--value")[2].get_text()
+    except:
+        d_data['level'] = ''
+        print(link)
+    try:
+        d_data['type'] = soup.find_all(class_="addetailslist--detail--value")[3].get_text()
+    except:
+        d_data['type'] = ''
+        print(link)
     d_data['furnishing'] = soup.find_all(id='viewad-configuration')
     try:
         d_data['furnishing'] = d_data['furnishing'][0].contents[3].text  # todo make list
     except:
         d_data['furnishing'] = ''
-    d_data['long_description'] = soup.find(id='viewad-description-text').get_text()
+        print(link)
+    try:
+        d_data['long_description'] = soup.find(id='viewad-description-text').get_text()
+    except:
+        d_data['long_description'] =''
 
     return d_data
 
@@ -74,9 +119,10 @@ list_compl_links = get_links(path_to_linklist,baselink)
 # df = pd.DataFrame(index=range(len(list_compl_links)), columns= list(d_data.keys()))
 
 beautiful_flats = []
+print("{cnt} beautiful flats found".format(cnt=len(list_compl_links)))
 for cnt in range(len(list_compl_links)):
     beautiful_flats.append(extract_info_d(get_soup(list_compl_links[cnt]),list_compl_links[cnt]))
-    time.sleep(randint(2, 20))
+    time.sleep(randint(2, 10))
     print(cnt)
     if cnt % 50 == 0:
         timetag = time.strftime("%Y%m%d-%H%M%S")
